42b5b7ded79d3b157c278d242c90608564b995b7
dreins <davynreinhard@gmail.com>
2022-12-08 10:24:46 +0700

    Added Harapan Page

--- lib/page/harapan_page.dart
+++ lib/page/harapan_page.dart
@@ -1,5 +1,8 @@
+import 'package:do_nasi/widgets/harapan_carousel.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class HarapanPage extends StatefulWidget {
   const HarapanPage({super.key});
@@ -11,6 +14,11 @@ class HarapanPage extends StatefulWidget {
 class _HarapanPageState extends State<HarapanPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Harapan'),
+      ),
+      body: Center(child: Column(children: [HarapanCarousel()])),
+    );
   }
 }
