1f3dc026e3f6a5f4096ff8dbcf5266432743a012
Dave Matthew Peter Lumban Tobing <dave.matthew@ui.ac.id>
2022-12-09 19:44:54 +0700

    bagi role

--- lib/page/page_overview.dart
+++ lib/page/page_overview.dart
@@ -30,30 +30,96 @@ class _PageOverviewState extends State<PageOverview> {
           title: const Text('Page Overview'),
         ),
         body: FutureBuilder(
-            future: fetchPageOverview(request),
-            builder: (context, AsyncSnapshot snapshot) {
-              if (snapshot.data == null) {
-                return const Center(child: CircularProgressIndicator());
-              } else {
-                if (!snapshot.hasData) {
-                  return Column(
-                    children: const [
-                      Text(
-                        "Tidak ada Page :(",
-                        style:
-                            TextStyle(color: Color(0xff59A5D8), fontSize: 20),
-                      ),
-                      SizedBox(height: 8),
-                    ],
-                  );
-                } else {
-                  return ListView.builder(
-                      itemCount: snapshot.data!.length,
-                      itemBuilder: (_, index) {
-                        if (index == snapshot.data!.length - 1) {
-                          return Column(
-                            children: [
-                              Container(
+          future: request.get("https://do-nasi.up.railway.app/auth/get_user_json/"),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.data != null){
+              final is_penyalur = snapshot.data['data']['role'] == "Penyalur" ? true : false ;
+              return FutureBuilder(
+                  future: fetchPageOverview(request),
+                  builder: (context, AsyncSnapshot snapshot) {
+                    if (snapshot.data == null) {
+                      return const Center(child: CircularProgressIndicator());
+                    } else {
+                      if (!snapshot.hasData) {
+                        return Column(
+                          children: const [
+                            Text(
+                              "Tidak ada Page :(",
+                              style:
+                              TextStyle(color: Color(0xff59A5D8), fontSize: 20),
+                            ),
+                            SizedBox(height: 8),
+                          ],
+                        );
+                      } else {
+                        return ListView.builder(
+                            itemCount: snapshot.data!.length,
+                            itemBuilder: (_, index) {
+                              if (index == snapshot.data!.length - 1 && is_penyalur) {
+                                return Column(
+                                  children: [
+                                    Container(
+                                        margin: const EdgeInsets.symmetric(
+                                            horizontal: 16, vertical: 12),
+                                        padding: const EdgeInsets.all(20.0),
+                                        decoration: BoxDecoration(
+                                            color: Colors.white,
+                                            borderRadius: BorderRadius.circular(15.0),
+                                            boxShadow: const [
+                                              BoxShadow(
+                                                  color: Colors.black,
+                                                  blurRadius: 2.0)
+                                            ]),
+                                        child: Column(children: [
+                                          Row(
+                                            children: [
+                                              const Text(
+                                                "Title:",
+                                                style: TextStyle(
+                                                    fontWeight: FontWeight.bold),
+                                              ),
+                                              Flexible(
+                                                  child: Text(snapshot
+                                                      .data![index].fields.title))
+                                            ],
+                                          ),
+                                          Row(
+                                            children: [
+                                              const Text(
+                                                "Description:",
+                                                style: TextStyle(
+                                                    fontWeight: FontWeight.bold),
+                                              ),
+                                              Flexible(
+                                                  child: Text(snapshot.data![index]
+                                                      .fields.description))
+                                            ],
+                                          ),
+                                          Row(children: [
+                                            const Text(
+                                              "Deadline:",
+                                              style: TextStyle(
+                                                  fontWeight: FontWeight.bold),
+                                            ),
+                                            Flexible(
+                                                child: Text(
+                                                    DateFormat('EEEE, MMM d, yyyy')
+                                                        .format(snapshot.data![index]
+                                                        .fields.deadline)))
+                                          ]),
+                                        ])),
+                                    ElevatedButton(
+                                        onPressed: () {
+                                          Navigator.push(
+                                              context,
+                                              MaterialPageRoute(
+                                                  builder: (context) => MyFormPage(setparent: refresh)));
+                                        },
+                                        child: const Text('TAMBAH DONASI'))
+                                  ],
+                                );
+                              }
+                              return Container(
                                   margin: const EdgeInsets.symmetric(
                                       horizontal: 16, vertical: 12),
                                   padding: const EdgeInsets.all(20.0),
@@ -62,108 +128,53 @@ class _PageOverviewState extends State<PageOverview> {
                                       borderRadius: BorderRadius.circular(15.0),
                                       boxShadow: const [
                                         BoxShadow(
-                                            color: Colors.black,
-                                            blurRadius: 2.0)
+                                            color: Colors.black, blurRadius: 2.0)
                                       ]),
                                   child: Column(children: [
                                     Row(
                                       children: [
                                         const Text(
-                                          "Title",
-                                          style: TextStyle(
-                                              fontWeight: FontWeight.bold),
+                                          "Title ",
+                                          style:
+                                          TextStyle(fontWeight: FontWeight.bold),
                                         ),
                                         Flexible(
-                                            child: Text(snapshot
-                                                .data![index].fields.title))
+                                            child: Text(
+                                                snapshot.data![index].fields.title))
                                       ],
                                     ),
                                     Row(
                                       children: [
                                         const Text(
-                                          "Description",
-                                          style: TextStyle(
-                                              fontWeight: FontWeight.bold),
+                                          "Description ",
+                                          style:
+                                          TextStyle(fontWeight: FontWeight.bold),
                                         ),
                                         Flexible(
-                                            child: Text(snapshot.data![index]
-                                                .fields.description))
+                                            child: Text(snapshot
+                                                .data![index].fields.description))
                                       ],
                                     ),
                                     Row(children: [
                                       const Text(
                                         "Deadline ",
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.bold),
+                                        style: TextStyle(fontWeight: FontWeight.bold),
                                       ),
                                       Flexible(
-                                          child: Text(
-                                              DateFormat('EEEE, MMM d, yyyy')
-                                                  .format(snapshot.data![index]
-                                                      .fields.deadline)))
+                                          child: Text(DateFormat('EEEE, MMM d, yyyy')
+                                              .format(snapshot
+                                              .data![index].fields.deadline)))
                                     ]),
-                                  ])),
-                              ElevatedButton(
-                                  onPressed: () {
-                                    Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) => MyFormPage(setparent: refresh)));
-                                  },
-                                  child: const Text('TAMBAH DONASI'))
-                            ],
-                          );
-                        }
-                        return Container(
-                            margin: const EdgeInsets.symmetric(
-                                horizontal: 16, vertical: 12),
-                            padding: const EdgeInsets.all(20.0),
-                            decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(15.0),
-                                boxShadow: const [
-                                  BoxShadow(
-                                      color: Colors.black, blurRadius: 2.0)
-                                ]),
-                            child: Column(children: [
-                              Row(
-                                children: [
-                                  const Text(
-                                    "Title ",
-                                    style:
-                                        TextStyle(fontWeight: FontWeight.bold),
-                                  ),
-                                  Flexible(
-                                      child: Text(
-                                          snapshot.data![index].fields.title))
-                                ],
-                              ),
-                              Row(
-                                children: [
-                                  const Text(
-                                    "Description ",
-                                    style:
-                                        TextStyle(fontWeight: FontWeight.bold),
-                                  ),
-                                  Flexible(
-                                      child: Text(snapshot
-                                          .data![index].fields.description))
-                                ],
-                              ),
-                              Row(children: [
-                                const Text(
-                                  "Deadline ",
-                                  style: TextStyle(fontWeight: FontWeight.bold),
-                                ),
-                                Flexible(
-                                    child: Text(DateFormat('EEEE, MMM d, yyyy')
-                                        .format(snapshot
-                                            .data![index].fields.deadline)))
-                              ]),
-                            ]));
-                      });
-                }
-              }
-            }));
+                                  ]));
+                            });
+                      }
+                    }
+                  });
+            }
+            else{
+              return const Center(child: CircularProgressIndicator());
+            }
+          } ,
+        ));
   }
 }
